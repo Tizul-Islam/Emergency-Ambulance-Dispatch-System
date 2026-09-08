cf66ea616e1375d64a788be4a2f80d1513f6fefd
tizul-islam <tizulislamtt@gmail.com>
2026-09-08 12:10:09 +0600

    chore(style): format tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/server.ts"],
-  format: ["esm", "cjs"],
-  target: "esnext",
-  outDir: "dist",
+  entry: ['src/server.ts'],
+  format: ['esm', 'cjs'],
+  target: 'esnext',
+  outDir: 'dist',
   clean: true,
   bundle: true,
   splitting: false,
