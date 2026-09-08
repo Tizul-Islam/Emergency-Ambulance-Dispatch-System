7cc5a4b87f0684cb2f29d71ea9fd157f3dcd0e9c
tizul-islam <tizulislamtt@gmail.com>
2026-09-08 12:03:10 +0600

    fix(vercel): bypass app.listen when running in Vercel serverless environment

--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,23 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+let server: any;
+if (!process.env.VERCEL) {
+  server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 process.on('unhandledRejection', (err: any) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
-  server.close(() => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
     process.exit(1);
-  });
+  }
 });
+
+export default app;
